5070fe0d13411d3417ae2b23873bd048d390f048
Greg_House_M_D <maxim.paluh@gmail.com>
2024-01-13 01:18:42 +0200

    New LogInstaller

--- LogInstall/Program.cs
+++ LogInstall/Program.cs
@@ -1,76 +1,80 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Text.RegularExpressions;
-using System.Xml.Linq;
-using LogInstall;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-string CompasPackLogName = "CompasPackLog";
-var CompasPackLog = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)+"\\"+ CompasPackLogName;
-var CurrentDirectory = Directory.GetCurrentDirectory();
-var Root = Path.GetPathRoot(CurrentDirectory);
-var AidaPath = Path.Combine(Root, "!Portable\\AIDA64");
-var typeReport = TypeReport.Unknown;
-
-if (!Directory.Exists(CompasPackLog))
-    Directory.CreateDirectory(CompasPackLog);
-
-var _fileName = DateTime.Now.ToString().Replace(':', '.').Replace(' ', '_');
-
-if (args.Any(x => x.Contains("\\LogInstall", StringComparison.InvariantCultureIgnoreCase)))
-    typeReport = TypeReport.LogInstall;
-if (args.Any(x => x.Contains("\\Report", StringComparison.InvariantCultureIgnoreCase)))
-    typeReport = TypeReport.Report;
-
-if (typeReport == TypeReport.LogInstall)
+namespace LogInstall
 {
-    var name = args.FirstOrDefault(x => x.Contains("Name:", StringComparison.InvariantCultureIgnoreCase));
-    if (!string.IsNullOrWhiteSpace(name))
-        _fileName = new Regex("Name:").Replace(name, "") + "_" + _fileName;
-}
-
-
-
-switch (typeReport)
-{
-    case TypeReport.Unknown:
-        Console.WriteLine($"Не вказано тип звiту!!!");
-        break;
-    case TypeReport.LogInstall:
+    internal class Program
+    {
+        static void Main(string[] args)
         {
-            var proc = new ProcessStartInfo()
+            var CompasPackLogFile = string.Empty;
+            try
             {
-                UseShellExecute = false,
-                FileName = AidaPath + "\\aida64.exe",
-                Arguments = "/R " + CompasPackLog + $"\\{"LogInstall_" +_fileName}. " + "/HTML " + "/CUSTOM " + AidaPath + "\\ForLog.rpf",
-            };
-
-            var process = Process.Start(proc);
-            Console.WriteLine("Запущено генерацiю звiту з такими параметрами:");
-            Console.WriteLine($"FileName: {proc.FileName}");
-            Console.WriteLine($"Arguments: {proc.Arguments}");
-            process.WaitForExit();
-            Console.WriteLine($"Готово!!!");
+                var name = args.FirstOrDefault();
+                var CompasPackLog = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "CompasPackLog");
+                CompasPackLogFile = Path.Combine(CompasPackLog, name != null ? $"LogInstall_{name}." : $"LogInstall_{DateTime.Now.ToString().Replace(':', '.').Replace(' ', '_')}.");
+                var CurrentDirectory = Directory.GetCurrentDirectory();
+                var Root = Path.GetPathRoot(CurrentDirectory);
+                var aidaExe = Path.Combine(Root, ReadAllTextAsync("AidaPath.txt"));
+                var aidaRPF = Path.Combine(Path.GetDirectoryName(aidaExe), "ForLog.rpf");
+                GetAidaReport(aidaExe, CompasPackLogFile, "/HML", aidaRPF);
+                Thread.Sleep(1000);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"В процесi формування звiту AIDA64 вiдбулася помилка:\n{ex.Message}\nФайл: {CompasPackLogFile}hml не створено!");
+                Thread.Sleep(10000);
+            }
         }
-        break;
-    case TypeReport.Report:
+
+        public static void GetAidaReport(string aidaExeFilePath, string reportPath, string type, string rpfPath)
         {
-            var proc = new ProcessStartInfo()
+            if (string.IsNullOrWhiteSpace(aidaExeFilePath))
+                throw new ArgumentException($"{nameof(aidaExeFilePath)} Is Null Or White Space");
+
+            if (string.IsNullOrWhiteSpace(reportPath))
+                throw new ArgumentException($"{nameof(reportPath)} Is Null Or White Space");
+
+            if (string.IsNullOrWhiteSpace(rpfPath))
+                throw new ArgumentException($"{nameof(rpfPath)} Is Null Or White Space");
+
+
+            Process proc = null;
+            ProcessStartInfo StartInfo = new ProcessStartInfo
             {
-                UseShellExecute = false,
-                FileName = AidaPath + "\\aida64.exe",
-                Arguments = "/R " + CompasPackLog + "\\Report. " + "/XML " + "/CUSTOM " + AidaPath + "\\ForReport.rpf",
+                FileName = aidaExeFilePath,
+                Arguments = $"/R {reportPath} {type} /CUSTOM {rpfPath}",
+                UseShellExecute = false
             };
-
-            var process = Process.Start(proc);
-            Console.WriteLine("Запущено генерацiю звiту з такими параметрами:");
-            Console.WriteLine($"FileName: {proc.FileName}");
-            Console.WriteLine($"Arguments: {proc.Arguments}");
-            process.WaitForExit();
-            Console.WriteLine($"Готово!!!");
+            try
+            {
+                proc = Process.Start(StartInfo);// TODO
+                Console.WriteLine("Запущено генерацiю звiту з такими параметрами:");
+                Console.WriteLine($"FileName: {StartInfo.FileName}");
+                Console.WriteLine($"Arguments: {StartInfo.Arguments}");
+                if (!proc.WaitForExit(60000))
+                {
+                    throw new Exception("Aida report time out");
+                }
+                Console.WriteLine($"Готово!!!");
+            }
+            catch (Exception)
+            {
+                try { proc?.Kill(); } catch (Exception) { }
+                try { proc?.Close(); } catch (Exception) { }
+                throw;
+            }
         }
-        break;
-    default:
-        Console.WriteLine($"Не вказано тип звiту!!!");
-        break;
-}
 
+        public static string ReadAllTextAsync(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
---
+++ LogInstallNet/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using LogInstall;
+
+var CompasPackLog = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "CompasPackLog");
+var CurrentDirectory = Directory.GetCurrentDirectory();
+var Root = Path.GetPathRoot(CurrentDirectory);
+var AidaPath = Path.Combine(Root, "!Portable\\AIDA64");
+var typeReport = TypeReport.Unknown;
+
+if (!Directory.Exists(CompasPackLog))
+    Directory.CreateDirectory(CompasPackLog);
+
+var _fileName = DateTime.Now.ToString().Replace(':', '.').Replace(' ', '_');
+
+if (args.Any(x => x.Contains("\\LogInstall", StringComparison.InvariantCultureIgnoreCase)))
+    typeReport = TypeReport.LogInstall;
+if (args.Any(x => x.Contains("\\Report", StringComparison.InvariantCultureIgnoreCase)))
+    typeReport = TypeReport.Report;
+
+if (typeReport == TypeReport.LogInstall)
+{
+    var name = args.FirstOrDefault(x => x.Contains("Name:", StringComparison.InvariantCultureIgnoreCase));
+    if (!string.IsNullOrWhiteSpace(name))
+        _fileName = new Regex("Name:").Replace(name, "") + "_" + _fileName;
+}
+
+
+
+switch (typeReport)
+{
+    case TypeReport.Unknown:
+        Console.WriteLine($"Не вказано тип звiту!!!");
+        break;
+    case TypeReport.LogInstall:
+        {
+            var proc = new ProcessStartInfo()
+            {
+                UseShellExecute = false,
+                FileName = AidaPath + "\\aida64.exe",
+                Arguments = "/R " + CompasPackLog + $"\\{"LogInstall_" +_fileName}. " + "/HTML " + "/CUSTOM " + AidaPath + "\\ForLog.rpf",
+            };
+
+            var process = Process.Start(proc);
+            Console.WriteLine("Запущено генерацiю звiту з такими параметрами:");
+            Console.WriteLine($"FileName: {proc.FileName}");
+            Console.WriteLine($"Arguments: {proc.Arguments}");
+            process.WaitForExit();
+            Console.WriteLine($"Готово!!!");
+        }
+        break;
+    case TypeReport.Report:
+        {
+            var proc = new ProcessStartInfo()
+            {
+                UseShellExecute = false,
+                FileName = AidaPath + "\\aida64.exe",
+                Arguments = "/R " + CompasPackLog + "\\Report. " + "/XML " + "/CUSTOM " + AidaPath + "\\ForReport.rpf",
+            };
+
+            var process = Process.Start(proc);
+            Console.WriteLine("Запущено генерацiю звiту з такими параметрами:");
+            Console.WriteLine($"FileName: {proc.FileName}");
+            Console.WriteLine($"Arguments: {proc.Arguments}");
+            process.WaitForExit();
+            Console.WriteLine($"Готово!!!");
+        }
+        break;
+    default:
+        Console.WriteLine($"Не вказано тип звiту!!!");
+        break;
+}
+
